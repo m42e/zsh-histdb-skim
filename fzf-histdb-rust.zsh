8d60656aee39a024e73bc2ad1c4ca8bd1f6648d5
Matthias Bilger <matthias@bilger.info>
2022-03-29 14:52:55 +0200

    added download function for release

--- fzf-histdb-rust.zsh
+++ fzf-histdb-rust.zsh
@@ -1,3 +1,27 @@
+histdb-skim-get-os(){
+  UNAME_STR=`uname -a`
+  if [[ ( $UNAME_STR =~ '.*Darwin.*' ) && ( $UNAME_STR =~ '.*x86_64.*') ]]; then
+    echo -n "darwin-x64"
+  fi
+  if [[ ( $UNAME_STR =~ '.*Linux.*' ) && ( $UNAME_STR =~ '.*x86_64.*') ]]; then
+    echo -n "linux-x64"
+  fi
+}
+
+histdb-skim-get-latest-version(){
+  curl -s https://github.com/m42e/zsh-histdb-skim/releases/latest | grep  --color=never -o 'v[0-9]*\.[0-9]*\.[0-9]*'
+}
+
+histdb-skim-download(){
+  if [[ -z $(histdb-skim-get-os) ]]; then
+    echo "Could not find prebuild executable"
+    echo "You have to do it yourself"
+  else
+    curl -s -JLO https://github.com/m42e/zsh-histdb-skim/releases/download/$(histdb-skim-get-latest-version)/zsh-histdb-skim-$(histdb-skim-get-os)
+  fi
+}
+
+
 histdb-fzf-widget() {
   origquery=${BUFFER}
   output=$( \
